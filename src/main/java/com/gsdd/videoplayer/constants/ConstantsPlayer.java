1e5ac10bf4bc20692359e9f408ad5e5bc40a5ac8
Alex Galvis <alex.galvis.sistemas@gmail.com>
2021-12-14 12:39:14 -0500

    Update project structure

---
+++ src/main/java/com/gsdd/videoplayer/constants/ConstantsPlayer.java
@@ -0,0 +1,147 @@
+package com.gsdd.videoplayer.constants;
+
+import java.io.File;
+import java.util.regex.Matcher;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * 
+ * The main objective of this class is to store the basic constants needed for the application to
+ * work with.
+ * 
+ * @author Great System Development Dynamic [GSDD] <br>
+ *         Alexander Galvis Grisales <br>
+ *         alex.galvis.sistemas@gmail.com <br>
+ * 
+ * @since 1.0
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ConstantsPlayer {
+
+  /**
+   * This property is for scan a found the required lib for VLC. (This is the path for libvlc.dll)
+   */
+  public static final String VLC_LIB_PATH = validateRoute("D:/Programas/VideoLAN/VLC/");
+
+  // Properties for titles of the menu and menuitem in aplication.
+
+  /**
+   * Text definition for admon menu.
+   */
+  public static final String MENU_TITLE = "Admon";
+
+  /**
+   * Text definition for menu item file.
+   */
+  public static final String MENU_ITEM_FILE = "Choose File";
+
+  /**
+   * Text definition for menu item mute.
+   */
+  public static final String MENU_ITEM_MUTE = "Mute/Un-Mute Current Video";
+
+  /**
+   * Text definition for menu item stop.
+   */
+  public static final String MENU_ITEM_STOP = "Stop Current Video";
+
+  /**
+   * Text definition for menu item out.
+   */
+  public static final String MENU_ITEM_OUT = "Exit Program";
+
+  /**
+   * File route to image icon for the window.
+   */
+  public static final String IMAGE_MENU = "/images/videoPlayer-Icon.png";
+
+  /**
+   * Logger when video has stopped.
+   */
+  public static final String LOGGER_VIDEO_STOP = "Stopping current video...";
+
+  /**
+   * Logger when exit.
+   */
+  public static final String LOGGER_EXIT = "Exit press in menu...";
+
+  // Chooser properties
+
+  /**
+   * Initial route for the chooser. (Current directory).
+   */
+  public static final String CHOOSE_ROOT = ".";
+
+  /**
+   * Title for chooser window.
+   */
+  public static final String CHOOSE_VIDEO = "Please select the video:";
+
+  /**
+   * Chooser filtered files, only allows to select a file with ext like description.
+   */
+  public static final String CHOOSE_DESC = "FileInput filtered: avi, wmv, mkv, mp4, flv, rmvb";
+
+  /**
+   * Array of ext filter to be implemented in the chooser.
+   */
+  protected static final String[] FILTER_INPUT = {"avi", "wmv", "mkv", "mp4", "flv", "rmvb"};
+
+  // This properties will be used to take and store snapshot.
+
+  /**
+   * Time between captures/snapshots (In milliseconds).
+   */
+  public static final long TIME_CAPTURES = 200;
+
+  /**
+   * A maximum number of captures/snapshots.
+   */
+  public static final int CAPTURES = 250;
+
+  /**
+   * Prefix for store the captures.
+   */
+  public static final String SNAPSHOT_PREFIX = "Capture_";
+
+  /**
+   * Extension for the captures.
+   */
+  public static final String SNAPSHOT_EXT = ".png";
+
+  /**
+   * Expresion to normalize routes.
+   */
+  public static final String EXPRESION_SLASH = "[\\/]";
+
+  /**
+   * File route out if not defined will be the user home.
+   */
+  public static final String ROUTE_OUT = System.getProperty("user.home");
+
+  /**
+   * 
+   * This method allows to get a route even with SO differences.
+   * 
+   * @since 1.0
+   * @param route the route to normalize.
+   * @return a normalized route.
+   */
+
+  public static String validateRoute(String route) {
+    String r = route.replaceAll(EXPRESION_SLASH, Matcher.quoteReplacement(File.separator));
+    if (!r.endsWith(File.separator)) {
+      r += File.separator;
+    }
+    return r;
+  }
+
+  /**
+   * @return the filterInput
+   */
+  public static String[] getFilterInput() {
+    return FILTER_INPUT;
+  }
+
+}
---
+++ src/main/java/com/gsdd/videoplayer/controller/VLCPlayerController.java
@@ -0,0 +1,150 @@
+package com.gsdd.videoplayer.controller;
+
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import com.gsdd.videoplayer.constants.ConstantsPlayer;
+import com.gsdd.videoplayer.model.SnapThread;
+import com.gsdd.videoplayer.view.VLCPlayerView;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
+
+/**
+ * This class acts like a controller that get the resources of the model ( {@link VLCPlayer}) and
+ * interacts with view ({@link VLCPlayerView}) to accomplished the objectives of the project.
+ * 
+ * @author Great System Development Dynamic [GSDD] <br>
+ *         Alexander Galvis Grisales <br>
+ *         alex.galvis.sistemas@gmail.com <br>
+ * @since 1.0
+ */
+@Slf4j
+@Getter
+@Setter
+public class VLCPlayerController {
+
+  /**
+   * Define the model to be used in the application.
+   */
+  private EmbeddedMediaPlayerComponent mediaPlayerComponent;
+  /**
+   * Define a thread for take the snapshots.
+   */
+  private SnapThread snap;
+  /**
+   * Define the sleep time.
+   */
+  private Long sleep;
+
+  /**
+   * A public construct method. Initialize the view and the model for work with.
+   * 
+   * @author Alexander Galvis
+   * @since 1.0
+   * @see VLCPlayer
+   */
+  public VLCPlayerController() {
+    mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
+  }
+
+  /**
+   * This method allows to choose a video file {@link ConstantsPlayer#FILTER_INPUT} and if it is a
+   * valid video file begin the process to take the required snapshot.
+   * <p>
+   * If nothing choose , nothing will happen.
+   * 
+   * @since 1.0
+   */
+  public void chooseFile() {
+    JFileChooser fs = new JFileChooser();
+    fs.setCurrentDirectory(new File(ConstantsPlayer.CHOOSE_ROOT));
+    fs.setDialogTitle(ConstantsPlayer.CHOOSE_VIDEO);
+    fs.setFileSelectionMode(JFileChooser.FILES_ONLY);
+    fs.setAcceptAllFileFilterUsed(false);
+    FileNameExtensionFilter filter =
+        new FileNameExtensionFilter(ConstantsPlayer.CHOOSE_DESC, ConstantsPlayer.getFilterInput());
+    fs.setFileFilter(filter);
+    if (fs.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+      log.info(fs.getSelectedFile().getAbsolutePath());
+      playVideoFromRoute(fs.getSelectedFile().getAbsolutePath());
+    }
+  }
+
+  /**
+   * This method allows to play the video of the selected route and while play the media can take
+   * the snapshot.
+   * 
+   * @author Alexander Galvis
+   * @since 1.0
+   * @param route an absolute path to the video resource.
+   */
+  private void playVideoFromRoute(String route) {
+    Boolean play = mediaPlayerComponent.getMediaPlayer().playMedia(route);
+    log.info("{}:::{}", route, play);
+    log.info("Time: {}", mediaPlayerComponent.getMediaPlayer().getTime());
+    takeSnapshot(route);
+  }
+
+  /**
+   * This method creates an instance of {@link SnapThread} to take the snapshot required, if there
+   * is already a definition of the thread that will be stopped and create a new one.
+   * 
+   * @author Alexander Galvis
+   * @since 1.0
+   * @param route the audio/video file absolute path.
+   */
+  private void takeSnapshot(String route) {
+    if (snap != null) {
+      snap.interrupt();
+    }
+    snap = new SnapThread(route, sleep, this);
+    snap.start();
+  }
+
+  /**
+   * This method allows to stop the current playing video, also if the {@link SnapThread} is taking
+   * the captures will be interrupted. If there is not video playing nothign will happen.
+   * 
+   * @since 1.0
+   * @see SnapThread
+   */
+  public void stopVideo() {
+    if (mediaPlayerComponent.getMediaPlayer().isPlaying()) {
+      log.info(ConstantsPlayer.LOGGER_VIDEO_STOP);
+      mediaPlayerComponent.getMediaPlayer().stop();
+      if (snap != null) {
+        snap.interrupt();
+      }
+    }
+  }
+
+  /**
+   * This method allows to mute/un-mute the current video playing. If the video is not currently
+   * playing nothing will happen. If the current video is mute the action will be un-mute.
+   * 
+   * @since 1.0
+   */
+  public void muteVideo() {
+    if (mediaPlayerComponent.getMediaPlayer().isPlaying()) {
+      if (mediaPlayerComponent.getMediaPlayer().isMute()) {
+        mediaPlayerComponent.getMediaPlayer().mute(Boolean.FALSE);
+      } else {
+        mediaPlayerComponent.getMediaPlayer().mute(Boolean.TRUE);
+      }
+    }
+  }
+
+  /**
+   * This method allows to exit of the application. Stop current actions and exit program.
+   * 
+   * @since 1.0
+   */
+  public void exit() {
+    log.info(ConstantsPlayer.LOGGER_EXIT);
+    stopVideo();
+    System.exit(0);
+  }
+
+}
